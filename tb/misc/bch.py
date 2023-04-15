132aaa4838e9720ec8d9d70d7e40b7b7111e8c5f
hezlosliwy <hezlosliwy@op.pl>
2023-04-15 15:28:14 +0200

    add scripts for testing bch decoder and fir

---
+++ tb/misc/bch.py
@@ -0,0 +1,75 @@
+import galois
+import numpy as np
+
+# print(ccsds.field)
+# print(ccsds.generator_poly)
+# print(ccsds.is_systematic)
+# print(ccsds.G)
+# print(ccsds.roots)
+# print(bin(int('721', 16)))
+
+
+# print(gf6.primitive_element)
+# print(bin(gf6('x^1 + 1')**2))
+
+ccsds = galois.BCH(63, 51)
+gf6 = galois.GF(2**6)
+
+st = [i for i in range(63)]
+
+def find_error(err_pos):
+  r = ccsds.encode([1 for i in range(51)])
+  e = [0 for i in range(63)]
+  err_set = set(np.random.choice(st, 2))
+  for i in err_set:
+    e[i] = 1
+  # e[np.random.choice(st, 1)] = 1
+  # e[np.random.choice(st, 1)] = 1
+  r = r ^ e
+  # print(len(r))
+  a = gf6.primitive_element
+  N = 63
+  S1 = 0
+  for i in range(N):
+    if r[i]:
+      S1 = S1 ^ a**i
+  S2 = 0
+  for i in range(N):
+    if r[i]:
+      S2 = S2 ^ (a**2)**i
+  S3 = 0
+  for i in range(N):
+    if r[i]:
+      S3 = S3 ^ (a**3)**i
+  # print(S1, S2, S3)
+
+  L2 = S3 ^ S1*S2
+  L1 = S1*S2
+  L0 = S1
+
+  sig0 = 1
+  sig1 = S1
+  sig2 = S1**2 ^ S3*(S1**-1)
+  epos1 = []
+  for j in range(63):
+    if ( sig1*(a**-1)**j ^ sig2*((a**-1)**2)**j) == 1:
+      epos1.append(j)
+  epos1 = set(epos1)
+  if epos1 != err_set:
+    print("error :(")
+
+  # print(L2, L1, L0)
+  # epos = []
+  # for j in range(63):
+  #   if (L0 ^ L1*(a**-1)**j ^ L2*(a**(2))**j) == 0:
+  #     epos.append(j)
+      # break
+  # epos = ((epos & 0xFE) >> 1) | ((epos & 0x01)<<5)
+  print(f"real error pos {err_set}, found err pos {epos1}")
+  # print(f"real error pos {err_pos}, found err pos {epos}")
+  # print(a**epos)
+
+for i in range(62):
+  find_error(i)
+# S1 = S1 % [1, 0, 0, 0, 0, 1, 1]
+# (x6+x+1)(x6+x4+x2+x+1)
---
+++ tb/misc/fir_tb.py
@@ -0,0 +1,63 @@
+import commpy
+import numpy as np
+
+###
+import matplotlib.pyplot as plt
+###
+
+from fixedpoint import FixedPoint
+
+coefs = [-6.518263187043542e-19, -7.986557578197472e-04, -0.002095202987291, -0.003667107969085, -0.005116643415385, -0.005928729916395, -0.005574799504880, -0.003647417401953, 4.178988515525191e-18, 0.005140327380087, 0.011118819256345, 0.016883231769783, 0.021109413050609, 0.022415483902514, 0.019637994970885, 0.012126229259175, -8.833912267524514e-18, -0.015685124676703, -0.032911842822594, -0.048841462160068, -0.060120550944356, -0.063323725395885, -0.055474603586424, -0.034566931655455, 1.284346534816169e-17, 0.047150897274960, 0.104087211505337, 0.166488826877401, 0.228951469309608, 0.285607773393619, 0.330847986488195, 0.360037028336954, 0.370121787174637, 0.360037028336954, 0.330847986488195, 0.285607773393619, 0.228951469309608, 0.166488826877401, 0.104087211505337, 0.047150897274960, 1.284346534816169e-17, -0.034566931655455, -0.055474603586424, -0.063323725395885, -0.060120550944356, -0.048841462160068, -0.032911842822594, -0.015685124676703, -8.833912267524514e-18, 0.012126229259175, 0.019637994970885, 0.022415483902514, 0.021109413050609, 0.016883231769783, 0.011118819256345, 0.005140327380087, 4.178988515525191e-18, -0.003647417401953, -0.005574799504880, -0.005928729916395, -0.005116643415385, -0.003667107969085, -0.002095202987291, -7.986557578197472e-04]
+coefs_fxp = [FixedPoint(i, True, m=1, n=29) for i in coefs]
+
+for i in coefs:
+    print(bin(FixedPoint(i, True, m=1, n=29))[2:].zfill(30))
+    
+    
+
+def save_vector_to_file(vect, f_name):
+    f = open(f_name, "wb")
+    print(len(vect[0]))
+    for i in range(len(vect[0])):
+        f.write(int(vect[0][i]).to_bytes(length=2, byteorder="big", signed=True)) #write i
+        f.write(int(vect[1][i]).to_bytes(length=2, byteorder="big", signed=True)) #write q
+    f.close()
+    
+fs = 10**7
+in_len = 10**3
+
+#create and normalize filter response
+
+# t_idx, h_rrc = commpy.filters.rrcosfilter(64, 0.35, 8/fs, fs)
+h_rrc = np.array(coefs)
+h_rrc = h_rrc/(sum([i**2 for i in coefs])/len(coefs))**0.5
+# h_rrc = np.array(h_rrc)/(sum(h_rrc**2)**0.5)
+# h_rrc = h_rrc/sum(np.abs(h_rrc))
+# print(h_rrc)
+# print(sum(np.abs(h_rrc)))
+# h_rrc = h_rrc / max(np.abs(np.fft.fft(h_rrc))) / 1.44
+
+plt.figure()
+plt.plot(h_rrc, "x")
+# plt.figure()
+# plt.plot(np.abs(np.fft.fft(h_rrc)), 'x-')
+# plt.show()
+
+# generate vector
+
+in_vect_i, in_vect_q = np.array([[i]*8 for i in np.random.choice([1.0, -1.0], size=in_len)]).flatten(), np.array([[i]*8 for i in np.random.choice([1.0, -1.0], size=in_len)]).flatten()
+
+out_vect_i, out_vect_q = np.correlate(in_vect_i, h_rrc), np.correlate(in_vect_q, h_rrc) # *(2**11-1) *(2**11-1)
+
+#save vectors
+
+save_vector_to_file([in_vect_i, in_vect_q], "vectors.in")
+save_vector_to_file([out_vect_i, out_vect_q], "vectors.out")
+
+# plt.figure()
+# plt.plot(in_vect_i, "x")
+
+# plt.figure()
+# plt.plot(np.abs(np.fft.fft(out_vect_i)), "x")
+
+# plt.show()
