ae78c105ef9de7f8f0c284bc1a0f14415a86fa4b
SzyIr <81175640+SzyIr@users.noreply.github.com>
2023-04-25 15:20:19 +0200

    update bch use state machine

--- src/rtl/bch_encoder.sv
+++ src/rtl/bch_encoder.sv
@@ -4,39 +4,82 @@ module bch_encoder(
         input wire clk,
         input wire rst,
         input wire ready_in,
+        output reg ready_out,
+        input wire valid_in,
+        output reg valid_out,
         input wire data_in,
         input wire [50:0] data_in_all,
-        output reg [62:0] data_out,
-        output reg ready
+        output reg [62:0] data_out_all,
+        output reg data_out
     );
     //reg [7:0] exp =  8'b11000101;
     //reg [12:0] exp =  13'b1010100111001;
+    typedef enum {IDLE, RESET, WORK, REST} state_t;
+    state_t state;
     int counter = 0;
-    reg [50:0] tmp = 0; 
+    reg [62:0] tmp = 0; 
     reg [12:0] rest = 0; 
-    int i =0;    
-    initial 
-        ready<=1;
-    always @ (posedge clk) begin
-        tmp[50-counter]<=data_in;
-        ready<=1;         
-        rest[0] <= data_in ^ rest[11]; 
-        rest[1] <= rest[0]; 
-        rest[2] <= rest[1];
-        rest[3] <= rest[2] ^ (data_in  ^ rest[11]);
-        rest[4] <= rest[3] ^ (data_in  ^ rest[11]);
-        rest[5] <= rest[4] ^ (data_in  ^ rest[11]);
-        rest[6] <= rest[5];
-        rest[7] <= rest[6];
-        rest[8] <= rest[7] ^ (data_in  ^ rest[11]);
-        rest[9] <= rest[8];
-        rest[10] <= rest[9] ^ (data_in  ^ rest[11]);
-        rest[11] <= rest[10];
-        counter <= (ready_in)?(counter + 1)%52:counter;
-        if(counter == 51)begin 
-            ready <=0;
-            data_out <= {tmp[50:0],rest[11:0]};
+ 
+    always @ (posedge clk or posedge rst ) begin
+        if(rst)begin
+            state <= IDLE;
+            counter <= 0;
+            tmp <= 0;
             rest <= 0;
+        end
+        else begin
+            case (state)
+                IDLE: begin
+                    if(valid_in && ready_in == 1)begin
+                        ready_out<=1;
+                        state<=WORK;
+                    end
+                end
+                WORK: begin
+                        counter <= (ready_in)?(counter + 1)%63:counter;
+                        ready_out<=(counter < 50)?1:0;
+                        state<=(counter < 50)?WORK:REST;
+                        
+                end
+                REST: begin
+                        ready_out<=0;
+                        state<=(counter <= 62)?REST:RESET;
+                        counter <= (ready_in)?(counter + 1):counter;
+                end
+                RESET: begin
+                        ready_out<=0;
+                        state<=IDLE;
+                        rest <=0;
+                        tmp<=0;
+                        counter<=0;
+                end
+                
+            endcase    
+        end    
+    end
+        
+    always @ (posedge clk) begin
+        if(state == WORK)begin
+            tmp[62-counter]<=data_in;
+            data_out <= data_in;
+            rest[0] <= data_in ^ rest[11]; 
+            rest[1] <= rest[0]; 
+            rest[2] <= rest[1];
+            rest[3] <= rest[2] ^ (data_in  ^ rest[11]);
+            rest[4] <= rest[3] ^ (data_in  ^ rest[11]);
+            rest[5] <= rest[4] ^ (data_in  ^ rest[11]);
+            rest[6] <= rest[5];
+            rest[7] <= rest[6];
+            rest[8] <= rest[7] ^ (data_in  ^ rest[11]);
+            rest[9] <= rest[8];
+            rest[10] <= rest[9] ^ (data_in  ^ rest[11]);
+            rest[11] <= rest[10];
+        end
+        else if(state == REST)begin
+            tmp[62-counter]<=rest[62-counter];
+            data_out_all <= {data_in_all[50:0],rest[11:0]};//all data out
+            data_out<=rest[62-counter];
+            rest[12:0]<=rest[12:0];
         end
          
     end
--- src/sim/encoder_tb.sv
+++ src/sim/encoder_tb.sv
@@ -5,22 +5,31 @@ module encoder_tb(
     reg clk =0;
     reg rst = 0; 
     reg data_in;
-    reg [50:0] data_in_all = 51'b010110101101001111001110100100000010101111010001100;
+    reg [50:0] data_in_all = 51'b011111100000110011101001010100011001001011110100100;
     //56'b11011100110101011101000100101000101010111010101011110101;
-    reg [62:0] data_out;
-    reg ready;
+    reg [62:0] data_out_all;
+    reg data_out;
+    reg ready_out;
     reg ready_in=0;
+    reg valid_out;
+    reg valid_in=0;
     always
         #5 clk <= ~clk;
     int i = 7'd50;  
       
+      initial begin
+            #10 ready_in=1;
+            #10 valid_in=1;
+      end
     always @(posedge clk) begin
-        if(ready)begin
-            ready_in=1;
+
+        if(ready_out)begin
+
             data_in = data_in_all[i];
             i = i-1;
         end
         if(i<0)begin
+            #10
             i = 7'd50;
             
         end
@@ -32,10 +41,14 @@ module encoder_tb(
     clk,
     rst,
     ready_in,
+    ready_out,
+    valid_in,
+    valid_out,
     data_in,
     data_in_all,
-    data_out,
-    ready
+    data_out_all,
+    data_out
     );
+
     
 endmodule
