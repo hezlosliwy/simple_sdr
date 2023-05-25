1049423f2af74476aa6aebd2e22e2cae45739712
Szymi <aghaghaghxdd@gmail.com>
2023-05-25 11:36:59 +0200

    check correct of decoder and decoder

--- src/sim/encoder_tb.sv
+++ src/sim/encoder_tb.sv
@@ -6,30 +6,32 @@ module encoder_tb(
     reg rst = 0; 
     reg data_in;
     reg [50:0] data_in_all = 51'b011011011110110110001111011001011110011001101100011;
-    //56'b11011100110101011101000100101000101010111010101011110101;
     reg [62:0] data_out_all;
-    reg data_out;
-    reg ready_out;
+    reg data_out,data_out2;
+    reg ready_out,ready_out2;
     reg ready_in=0;
     reg valid_out;
     reg valid_in=0;
+    reg out_data ;
+    reg out_valid ,out_ready;
+    reg [62:0] checkdata;
     always
         #5 clk <= ~clk;
     int i = 7'd50;  
       
       initial begin
-            #10 ready_in=1;
+             rst = 1; repeat(2)@(posedge clk);rst = 0;
+//           10 ready_in=1;
             #10 valid_in=1;
       end
     always @(posedge clk) begin
 
-        if(ready_out)begin
+        if(ready_in)begin
 
             data_in = data_in_all[i];
             i = i-1;
         end
         if(i<0)begin
-            #10
             i = 7'd50;
             
         end
@@ -49,6 +51,28 @@ module encoder_tb(
     data_out_all,
     data_out
     );
-
     
+    random_err random_err(
+    clk,
+    rst,
+    ready_out,
+    valid_out,
+    data_out,
+    data_out2
+    );
+    
+    bch_decoder bch_decoder(
+    clk,
+    rst,
+    valid_out,
+    data_out2,
+    ready_out,
+    out_valid,
+    out_data,
+    1'b1
+    );
+    
+     always @(posedge clk) begin
+        checkdata<={checkdata[61:0],out_data};
+     end
 endmodule
