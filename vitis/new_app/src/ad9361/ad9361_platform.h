ff200570440926c6effa3fc661a7c859bb1b38a7
Michal Szewczyk <szew.micha99@gmail.com>
2023-05-31 20:09:58 +0200

    platform init; app cleanup

---
+++ vitis/new_app/src/ad9361/ad9361_platform.h
@@ -0,0 +1,14 @@
+#ifndef AD9361_PLATFORM_H	
+#define AD9361_PLATFORM_H
+
+#include "xgpiops.h"
+#include "xparameters.h"
+#include "ad9361_api.h"
+#include "ad9361.h"
+#include "xil_printf.h"
+
+AD9361_InitParam default_init_param;
+AD9361_RXFIRConfig rx_fir_config;
+AD9361_TXFIRConfig tx_fir_config;
+
+#endif
